e3968d73cc0d82a203e1b8829135ed3c1dfda1f5
Pavel Degterev <pdegeterev@gmail.com>
2023-11-14 09:36:04 +0200

    finally !!!

--- Osa2/teht 2 ja 3.py
+++ Osa2/teht 2 ja 3.py
@@ -5,9 +5,12 @@ plt.style.use('dark_background')
 X = np.linspace(-np.pi * 3, np.pi * 3, 256, endpoint=True)
 C,S = np.cos(X), np.sin(X)
 fig = plt.figure(figsize=[4.8 * 3, 6.4 * 3])
-x_ticks = [-3 np.pi, -2pi, -pi, 0, pi, 2pi, 3pi]
+xticks_values = np.arange(-3 * np.pi, 3 * np.pi + 1e-9, np.pi / 2)
+xticks_labels = (r'$-3\pi$', r'$-5\pi/2$', r'$-2\pi$', r'$-3\pi/2$', r'$-\pi$',
+                 r'$-\pi/2$', '0', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$',
+                 r'$5\pi/2$', r'$3\pi$')
 
 plt.plot(X,C)
 plt.plot(X,S)
-
+plt.xticks(xticks_values,xticks_labels)
 plt.show()
