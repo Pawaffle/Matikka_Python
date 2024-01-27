735c48102f80bd828482f7cbb9665b9f44c993c1
Pavelishko <pdegterev@gmail.com>
2024-01-27 13:57:27 +0200

    27.01.24

--- Osa3/Teht_2.3.py
+++ Osa3/Teht_2.3.py
@@ -1,15 +1,9 @@
 import numpy as np
 
-# Sinulla on vektorit
-# a) u = 2i + 3j, v =4i - 7j
-# b) uu= i + j + k, vv 3i -3j + 2k.
-# Määrittele nämä numpy taulukon avulla.
+# Laske kunkin vektorin normi
 
-# Määritellään vektorit u ja v
 u = np.array([2, 3])
 v = np.array([4, -7])
-
-# Määritellään vektorit uu ja vv
 uu = np.array([1, 1, 1])
 vv = np.array([3, -3, 2])
 
